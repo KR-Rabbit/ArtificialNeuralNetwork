64eace9d19889be0006b42fc32a1f77fb84cdff5
Rmi <k_rabbit@163.com>
2024-01-12 22:32:38 +0800

    增加可供train.py调用的逻辑实现

--- val.py
+++ val.py
@@ -3,66 +3,81 @@ import argparse
 import numpy as np
 
 from model import NeuralNetwork
-from utils.confusion_matrix import con_matrix, score, plot
+from utils import get_root, increment_path
+from utils.confusion_matrix import *
 from utils.data import MNIST
 
+ROOT = get_root(__file__, 1)
 
-def main(opt):
+
+def run(data,
+        net=None,
+        bath_size=64,
+        save_dir=ROOT / "logs/val",
+        console=False,
+        file=False,
+        ):
     # 数据集
-    mnist = MNIST(root=opt.data, transform=[lambda x: x / 255.0,
+    if isinstance(data, MNIST):
+        mnist = data
+
+    else:
+        mnist = MNIST(root=data, transform=[lambda x: x / 255.0,
                                             lambda x: x.reshape(x.shape[0], -1)])  # transform 原始数据 范围0-255,shape 28x28
     test_images, test_labels = mnist.test_images, mnist.test_labels
-
     # 加载模型
-    net = NeuralNetwork()
-    net.detect()
-    net.load("./result/params.pkl")
+    training = net is not None
+    if not training:
+        net_path = net
+        net = NeuralNetwork()
+        net.load(net_path)
 
+    net.detect()
     # 测试
     y_trues = []
     y_preds = []
-    bath_size = 64
     batch_index = 0
+    mv_loss = 0.0
     while batch_index < len(test_images):
         batch_x = test_images[batch_index: batch_index + bath_size]
         batch_y = test_labels[batch_index: batch_index + bath_size]
         batch_index += bath_size
         y_trues.extend(batch_y)
-        y_preds.extend(net.predict(batch_x))
+        y_pred = net(batch_x)
+        loss = net.loss(y_pred, batch_y)
+        mv_loss = (mv_loss * batch_index + loss) / (batch_index + 1)
+        y_preds.extend(np.argmax(y_pred, axis=1))
     # 转为一维数组
     y_trues = np.array(y_trues).reshape(-1)
     y_preds = np.array(y_preds).reshape(-1)
     # 混淆矩阵
-    matrix = con_matrix(y_trues, y_preds, labels=[i for i in range(10)])
-    # 评估
-    acc, precision, recall, f1 = score(matrix)
-    if opt.print:
-        # 格式化标头，acc,precision,recall,对齐
-        formats_1 = "{:<10}" + "{:>14}" * 10
-        formats_2 = "{:<10}" + "{:>14.2f}" * 10
-        strings = [f"Label-{i}" for i in range(1, 11)]
-        print(formats_1.format(" ", *strings))
-        print(formats_2.format("precision", *precision))
-        print(formats_2.format("recall", *recall))
-        print(formats_2.format("f1", *f1))
-        print("{:<10}{:>14.2f}".format("acc-all", acc))
-    plot(matrix, save_dir=opt.save_dir, names=[i for i in range(10)])
+    matrix = get_confusion_matrix(y_trues, y_preds, labels=[i for i in range(10)])
+    # 评估 acc, precision, recall, f1
+    scores = get_score(matrix)
+    if not training:
+        save_dir = increment_path(save_dir)
+        save_dir.mkdir(parents=True, exist_ok=True)
+        if console or file:
+            log_scores(scores, console=console, file=file, save_dir=save_dir)
+        plot_confusionn_matrix(matrix, save_dir=save_dir, names=[i for i in range(10)])
+    return scores, mv_loss  # 返回评估结果
 
 
 def parser_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, default="./data", help="data root")
-    parser.add_argument("--print", action="store_true", help="print result of evaluation")
+    parser.add_argument("--net", type=str, required=True, help="net params path")
+    parser.add_argument("--batch-size", type=int, default=64, help="batch size")
+    parser.add_argument("--console", action="store_true", help="print result of evaluation")
+    parser.add_argument("--file", action="store_true", help="save result of evaluation to txt")
     parser.add_argument("--save_dir", type=str, default="./result", help="save dir")
     return parser.parse_args()
 
 
-def run(**kwargs):
-    opt = parser_opt()
-    opt.__dict__.update(kwargs)
-    main(opt)
+def main(opt):
+    main(**vars(opt))
 
 
 if __name__ == '__main__':
-    opt = parser_opt()
+    opt = parser_opt()  # 从命令行获取参数
     main(opt)
